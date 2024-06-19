fc1ba0b46c69cc2e0b8c062da25fd28c4907ae0a
David Ladaev <174save@gmail.com>
2024-06-19 18:11:34 +0300

    add slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,28 +1,30 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState, AppDispatch } from "../../context/store";
 import {Product} from '../../context/catalogSlice'
 import { fetchCatalog } from "../../context/catalogSlice";
 import Card from "../Card/Card";
 import SliderCss from "./Slider.module.css"
-
+import {Swiper, SwiperSlide} from "swiper/react"
+import { Pagination } from 'swiper/modules';
+import 'swiper/css/pagination';
+import 'swiper/css';
 
 const Slider = () =>{
-    const [numberOfProduts, setNumberOfProducts] = useState<Product[]>([])
     const sliderFlag = true
     const catalog: Product[] = useSelector((state: RootState)=>state.catalog.items)
     const dispatch = useDispatch<AppDispatch>()
     useEffect(()=>{
         dispatch(fetchCatalog())
-        let products = []
-        for(let i = 0; i<3;i++){
-            products.push(catalog[i])
-        }
-        setNumberOfProducts(products)
     },[dispatch])
     return(<>
         <section className={SliderCss.slider}>
-            {numberOfProduts.map((product)=>(<Card key={product.id} product={product} sliderFlag={sliderFlag}/>))}
+            <Swiper slidesPerView={3}  pagination={{clickable: true,}} modules={[Pagination]}>
+                {catalog.map((product)=>(
+                    <SwiperSlide className={SliderCss.card__item}>
+                        <Card key={product.id} product={product} sliderFlag={sliderFlag}/>
+                    </SwiperSlide>))}
+            </Swiper>
         </section>
     </>)
 }
