166f69e869546b87e55ad35b80bfec478378845c
David Ladaev <174save@gmail.com>
2024-06-30 22:43:50 +0300

    add data to profile page

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,19 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { setToken, fetchUserData } from '../../context/userSlice';
-import { AppDispatch } from '../../context/store';
+import { AppDispatch, RootState } from '../../context/store';
 import { Link } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import Slider from '../../components/Slider/Slider';
 import Google from '../../img/ico/google.png'
 import ProfileCss from './Profile.module.css'
+import { useSelector } from 'react-redux';
+import stub from '../../img/image-placeholder.png'
 
+interface Order {
+    status: string
+    id: number
+    createdAt: any
+    updatedAt: any
+    userId: string
+}
 
 const Profile = () => {
+    const [orders, setOrders] = useState<Order[]>([])
     const dispatch = useDispatch<AppDispatch>();
-
+    const user = useSelector((state: RootState)=>(state.user.user))
+    const userId = user.userId
+    console.log(orders, userId)
+    
+    const fetchOrders = async () => {
+        const response = await fetch(`http://31.128.39.49:80/api/orders/getOrders/${userId}`)
+        const data = await response.json()
+        setOrders(data)
+    }
+    
     useEffect(() => {
+        fetchOrders()
         const urlParams = new URLSearchParams(window.location.search);
         const accessToken = urlParams.get('accessToken');
         const refreshToken = urlParams.get('refreshToken');
@@ -28,22 +48,40 @@ const Profile = () => {
     return (
         <>
             <Header />
-            <div className={ProfileCss.links__container}>
-                <span>The profile page is still working, but you can use your Google account to add items to your <br/>cart
-                and favorites, as well as to place trial orders</span>
-                <a href="http://merchserver.org/api/user/auth/google" className={ProfileCss.google}>
-                    <div>
-                        <img src={Google} alt='google icon'/>
-                        <span>Google</span>
+            <div className={ProfileCss.profile__container}>
+                <div className={ProfileCss.user}>
+                    <img src={user.photo ? user.photo :stub} alt="userPhoto" />
+                    <div className={ProfileCss.user__bottom}>
+                        <div className={ProfileCss.user__data}>
+                            <span>{user.username}</span>
+                            <span>{user.email}</span>
+                        </div>
+                        <div>
+                            {orders.map(order=>(
+                                <div className={ProfileCss.order__data}>
+                                    <span>Order number: {order.id}</span>
+                                    <span>Order status: {order.status}</span>
+                                    <span>Order was created: {order.createdAt}</span>
+                                </div>
+                                ))}
+                        </div>
                     </div>
-                </a>
-                <Link to='/catalog'>
-                    <button className={`${ProfileCss.button} 
-                        ${ProfileCss.button_style} 
-                        ${ProfileCss.button_hover}`}
-                        >Go to catalog
-                    </button>
-                </Link>
+                </div>
+                <div className={ProfileCss.links__container}>
+                    <a href="http://merchserver.org/api/user/auth/google" className={ProfileCss.google}>
+                        <div>
+                            <img src={Google} alt='google icon'/>
+                            <span>Google</span>
+                        </div>
+                    </a>
+                    <Link to='/catalog'>
+                        <button className={`${ProfileCss.button} 
+                            ${ProfileCss.button_style} 
+                            ${ProfileCss.button_hover}`}
+                            >Go to catalog
+                        </button>
+                    </Link>
+                </div>
             </div>
             <Slider />
             <Footer />
