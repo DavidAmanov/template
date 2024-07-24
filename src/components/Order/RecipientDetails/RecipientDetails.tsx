0b5936ae6ae3fc8aca4116389441b27d9fdea22c
David Ladaev <174save@gmail.com>
2024-07-25 00:09:47 +0300

    add more fix

--- src/components/Order/RecipientDetails/RecipientDetails.tsx
+++ src/components/Order/RecipientDetails/RecipientDetails.tsx
@@ -1,84 +1,89 @@
-import RecipientDetailsCss from './RecipientDetailsCss.module.css'
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../../../context/store';
-import { resetRecipient, setRecipient } from '../../../context/orderSice';
-import { useEffect } from 'react';
-import { Recipient } from '../../../types/types';
-import Title from '../Title/Title';
+import RecipientDetailsCss from "./RecipientDetailsCss.module.css";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../../context/store";
+import { resetRecipient, setRecipient } from "../../../context/orderSice";
+import { useEffect } from "react";
+import { Recipient } from "../../../types/types";
+import Title from "../Title/Title";
 
 const RecipientDetails = () => {
-    const {register, handleSubmit} = useForm<Recipient>();
+  const { register, handleSubmit } = useForm<Recipient>();
 
-    const dispatch = useDispatch<AppDispatch>()
+  const dispatch = useDispatch<AppDispatch>();
 
-    useEffect(()=>{
-        dispatch(resetRecipient())
-    },[])
+  useEffect(() => {
+    dispatch(resetRecipient());
+  }, []);
 
-    const addRecipient: SubmitHandler<Recipient> = (data) => {
-        dispatch(setRecipient(data))
-    }
+  const addRecipient: SubmitHandler<Recipient> = (data) => {
+    dispatch(setRecipient(data));
+  };
 
-
-    return(<>
-        <div className={`${RecipientDetailsCss.block} ${RecipientDetailsCss.Recipient}`}>
-            <Title title='Recipient details' number={4} />
-            <div className={RecipientDetailsCss.block__wrapper}>
-                <div>Enter the details of the order recipient</div>
-                <form className={RecipientDetailsCss.FormWrapper} onSubmit={handleSubmit(addRecipient)}>
-                    <div className={RecipientDetailsCss.Recipient__details}>
-                        <div className={RecipientDetailsCss.Recipient__initials}>
-                            <div className={RecipientDetailsCss.Recipient__field}>
-                                <input {...register("name", {required: true })}
-                                    className={RecipientDetailsCss.Recipient__input} 
-                                    type="text" 
-                                    placeholder="Name"
-                                />
-                            </div>
-                            <div className={RecipientDetailsCss.Recipient__field}>
-                                <input {...register("lastName", {required: true })} 
-                                    className={RecipientDetailsCss.Recipient__input} 
-                                    type="text" 
-                                    placeholder="Last name"
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    <div className={RecipientDetailsCss.Recipient__details}>
-                        <div className={RecipientDetailsCss.Recipient__contacts}>
-                            <div className={RecipientDetailsCss.Recipient__field}>
-                                <input {...register("mobilePhone", {required: true })}
-                                    className={RecipientDetailsCss.Recipient__input} 
-                                    type="text" 
-                                    placeholder="Mobile Phone"
-                                />
-                            </div>
-                            <div className={RecipientDetailsCss.Recipient__field}>
-                                <input {...register("email", {required: true })} 
-                                    className={RecipientDetailsCss.Recipient__input} 
-                                    type="text" 
-                                    placeholder="Email" 
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    <div className={RecipientDetailsCss.Recipient__details}>
-                        <div className={RecipientDetailsCss.textarea}>
-                            <textarea {...register("comment", {required: false })} 
-                                className={RecipientDetailsCss.textarea__comment} 
-                                placeholder="Comment">    
-                            </textarea>
-                        </div>
-                    </div>
-                    <div className={RecipientDetailsCss.submitButtonField}>
-                        <button className={RecipientDetailsCss.submit__button} type="submit">Save</button>
-                    </div>
-                </form>
+  return (
+    <>
+      <div
+        className={`${RecipientDetailsCss.block} ${RecipientDetailsCss.Recipient}`}
+      >
+        <Title title="Recipient details" number={4} />
+        <div className={RecipientDetailsCss.block__wrapper}>
+          <div>Enter the details of the order recipient</div>
+          <form
+            className={RecipientDetailsCss.FormWrapper}
+            onSubmit={handleSubmit(addRecipient)}
+          >
+            <div className={RecipientDetailsCss.Recipient__field}>
+              <input
+                {...register("name", { required: true })}
+                className={RecipientDetailsCss.Recipient__input}
+                type="text"
+                placeholder="Name"
+              />
             </div>
+            <div className={RecipientDetailsCss.Recipient__field}>
+              <input
+                {...register("lastName", { required: true })}
+                className={RecipientDetailsCss.Recipient__input}
+                type="text"
+                placeholder="Last name"
+              />
+            </div>
+            <div className={RecipientDetailsCss.Recipient__field}>
+              <input
+                {...register("mobilePhone", { required: true })}
+                className={RecipientDetailsCss.Recipient__input}
+                type="text"
+                placeholder="Mobile Phone"
+              />
+            </div>
+            <div className={RecipientDetailsCss.Recipient__field}>
+              <input
+                {...register("email", { required: true })}
+                className={RecipientDetailsCss.Recipient__input}
+                type="email"
+                placeholder="Email"
+              />
+            </div>
+            <div className={RecipientDetailsCss.textarea}>
+              <textarea
+                {...register("comment", { required: false })}
+                className={RecipientDetailsCss.textarea__comment}
+                placeholder="Comment"
+              ></textarea>
+            </div>
+            <div className={RecipientDetailsCss.submitButtonField}>
+              <button
+                className={RecipientDetailsCss.submit__button}
+                type="submit"
+              >
+                Save
+              </button>
+            </div>
+          </form>
         </div>
-                        
-    </>)
-}
+      </div>
+    </>
+  );
+};
 
-export default RecipientDetails
+export default RecipientDetails;
