42b06d66cacc64abd8804ab5822a70aa62f1854f
David Ladaev <174save@gmail.com>
2024-07-25 23:14:59 +0300

    fix more css

--- src/components/FavouriteProduct/FavouriteProduct.tsx
+++ src/components/FavouriteProduct/FavouriteProduct.tsx
@@ -1,58 +1,62 @@
-import FavouriteProductCss from './FavouriteProductCss.module.css'
-import Trash from '../../img/ico/trash.png'
-import { Product } from '../../types/types'
-import { useDispatch } from 'react-redux'
-import { AppDispatch } from '../../context/store'
-import { removeProductFromFavourite } from '../../context/favouriteSlice'
-import ButtonRed from '../Button/ButtonRed'
-import { addProduct } from '../../context/cartSlice'
-import { useState } from 'react'
+import FavouriteProductCss from "./FavouriteProductCss.module.css";
+import Trash from "../../img/ico/trash.png";
+import { Product } from "../../types/types";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../context/store";
+import { removeProductFromFavourite } from "../../context/favouriteSlice";
+import ButtonRed from "../Button/ButtonRed";
+import { addProduct } from "../../context/cartSlice";
+import { useState } from "react";
 
 interface FavouriteProductProp {
-    product: Product
+  product: Product;
 }
 
-const FavouriteProduct: React.FC<FavouriteProductProp> = ({product}) => {
-    const [textToButton, setText] = useState<string>('Add to cart')
-    const dispatch = useDispatch<AppDispatch>()
+const FavouriteProduct: React.FC<FavouriteProductProp> = ({ product }) => {
+  const [textToButton, setText] = useState<string>("Add to cart");
+  const dispatch = useDispatch<AppDispatch>();
 
-    const handleRemove = () =>{
-        dispatch(removeProductFromFavourite({productId: product.id}))
-    }
+  const handleRemove = () => {
+    dispatch(removeProductFromFavourite({ productId: product.id }));
+  };
 
-    const addToCart = () => {
-        dispatch(addProduct({productId: product.id, quantity: 1}))
-        setText('Thank you!')
-    }
-    return(<>
-        <div className={FavouriteProductCss.cart__leftBlock}>
-            <div className={FavouriteProductCss.item}>
-                <div className={FavouriteProductCss.item__banner}>
-                    <div className={FavouriteProductCss.forImg}>
-                        <img className={FavouriteProductCss.img} src={product.img} alt="item to buy" />
-                    </div>
-                    <div className={FavouriteProductCss.item__info}>
-                        <span className={FavouriteProductCss.item__name}>{product.name}</span>
-                        <span className={FavouriteProductCss.item__size}>{product.description}</span>
-                    </div>
-                </div>
-                <div className={FavouriteProductCss.item__total}>
-                    <div className={FavouriteProductCss.item__left}> 
-                        <div className={FavouriteProductCss.item__price}>Price {product.price}</div>
-                        <div className={FavouriteProductCss.item__quantity}>Price for 1 piece.</div>
-                    </div>
-                    <ButtonRed onClick={addToCart} text={textToButton}/>
-                    <div className={FavouriteProductCss.item__right}>
-                        <button onClick={handleRemove}
-                            className={`${FavouriteProductCss.button} 
-                            ${FavouriteProductCss.button_cursor}`}>
-                                <img src={Trash} alt="Trash" />
-                        </button>
-                    </div>
-                </div>
-            </div>
+  const addToCart = () => {
+    dispatch(addProduct({ productId: product.id, quantity: 1 }));
+    setText("Thank you!");
+  };
+  return (
+    <div className={FavouriteProductCss.item}>
+      <div className={FavouriteProductCss.item__banner}>
+        <div className={FavouriteProductCss.forImg}>
+          <img
+            className={FavouriteProductCss.img}
+            src={product.img}
+            alt="item to buy"
+          />
         </div>
-    </>)
-}
+      </div>
+      <div className={FavouriteProductCss.item__info}>
+        <span className={FavouriteProductCss.item__name}>{product.name}</span>
+        <span className={FavouriteProductCss.item__size}>
+          {product.description}
+        </span>
+        <div className={FavouriteProductCss.item__info__div}>
+          <span className={FavouriteProductCss.item__price}>
+            Price {product.price}
+          </span>
+          <span className={FavouriteProductCss.item__quantity}>
+            Price for 1 piece.
+          </span>
+        </div>
+        <div className={FavouriteProductCss.button__div}>
+          <ButtonRed onClick={addToCart} text={textToButton} />
+          <button onClick={handleRemove} className={FavouriteProductCss.button}>
+            <img src={Trash} alt="Trash" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default FavouriteProduct
+export default FavouriteProduct;
--- src/components/Favourites/Favourites.tsx
+++ src/components/Favourites/Favourites.tsx
@@ -1,35 +1,36 @@
-import React from 'react'
-import FavouritesCss from './Favourites.module.css'
-import FavouriteProduct from '../FavouriteProduct/FavouriteProduct'
-import EmptyFavourites from '../EmptyFavourites/EmptyFavourites'
-import NavBar from '../NavBar/NavBar'
-import { CartProductType } from '../../types/types'
+import React from "react";
+import FavouritesCss from "./Favourites.module.css";
+import FavouriteProduct from "../FavouriteProduct/FavouriteProduct";
+import EmptyFavourites from "../EmptyFavourites/EmptyFavourites";
+import NavBar from "../NavBar/NavBar";
+import { CartProductType } from "../../types/types";
 
-interface FavProp{
-    isEmpty: boolean
-    favouriteProducts: CartProductType[]
+interface FavProp {
+  isEmpty: boolean;
+  favouriteProducts: CartProductType[];
 }
 
-const Favourites: React.FC<FavProp> = ({isEmpty, favouriteProducts}) => {
-    const linksArray = ['Home', 'Favourites']
-    const header = 'Favourites'
-    return(
-        <>
-            {isEmpty ? <EmptyFavourites /> 
-            :(<section className={FavouritesCss.section}>
-                <div className={FavouritesCss.section__wrapper}>
-                    <NavBar linksArray={linksArray} header={header}/>
-                    {favouriteProducts.map((item)=>(
-                        <FavouriteProduct 
-                            product={item.product} 
-                            key={item.id}
-                        />))}
-                </div>
-            </section>)}
-        </>
-    )
-}
+const Favourites: React.FC<FavProp> = ({ isEmpty, favouriteProducts }) => {
+  const linksArray = ["Home", "Favourites"];
+  const header = "Favourites";
+  return (
+    <>
+      {isEmpty ? (
+        <EmptyFavourites />
+      ) : (
+        <section className={FavouritesCss.section}>
+          <div className={FavouritesCss.section__wrapper}>
+            <NavBar linksArray={linksArray} header={header} />
+            <div className={FavouritesCss.section__wrapper__div}>
+              {favouriteProducts.map((item) => (
+                <FavouriteProduct product={item.product} key={item.id} />
+              ))}
+            </div>
+          </div>
+        </section>
+      )}
+    </>
+  );
+};
 
-export default Favourites
-
-
+export default Favourites;
